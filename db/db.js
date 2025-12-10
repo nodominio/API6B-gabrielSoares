2ddb9e18b1954c4be85fa5f1fa2d783a05e67acf
nodominio <dominiosemfundo@gmail.com>
2025-12-10 10:02:34 -0400

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,14 +1,16 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.dbconn
+const uri = process.env.dbconn;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 module.exports = (app) => {
     const client = new MongoClient(uri, {
         serverApi: {
             version: ServerApiVersion.v1,
             strict: true,
             deprecationErrors: true,
-        }
-    })
-    app.DBClient= client
-}
+        },
+        tls: true,
+        minTLSVersion: "TLSv1.2"
+    });
+
+    app.DBClient = client;
+};
