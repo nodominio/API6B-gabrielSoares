132e8f8f8ceb90172fed3ad899cf6375c0e451fe
nodominio <dominiosemfundo@gmail.com>
2025-12-10 16:46:44 -0400

    Update delnoticias.js

--- rotas/delnoticias.js
+++ rotas/delnoticias.js
@@ -1,14 +1,14 @@
-const { ObjectId } = require("mongodb");
+const { ObjectId } = require("mongodb")
 module.exports = (app) => {
-  app.delete("/delnoticias", async (req, res) => {
-    try {
-      const id = ObjectId.createFromHexString(req.body._id);
-      const resultado = await app.DBClient.db("portalnoticias")
-        .collection("noticias")
-        .deleteOne({ _id: id });
-      res.status(200).send("Usuário apagado");
-    } catch (error) {
-      res.status(400).send("Erro ao apagar o usuário: " + error);
-    }
-  });
-};
+    app.delete('/delnoticias', async (req, res) => {
+        try {
+            const id = ObjectId.createFromHexString(req.body._id)
+            const resultado = await app.DBClient.db('portalnoticias')
+                .collection('noticias')
+                .deleteOne({_id: id})
+            res.status(200).json({ status: 1, msg: "Notícia apagada com sucesso" })
+        } catch (error) {
+            res.status(400).json({ status: 0, msg: "Erro ao apagar notícia" })
+        }
+    })
+}
