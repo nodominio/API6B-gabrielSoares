298fe9a47fbd575d92a0bc756fc2952942d23537
nodominio <dominiosemfundo@gmail.com>
2025-12-10 16:18:38 -0400

    Add files via upload

--- rotas/delnoticias.js
+++ rotas/delnoticias.js
@@ -1,15 +1,14 @@
-const { ObjectId } = require("mongodb")
+const { ObjectId } = require("mongodb");
 module.exports = (app) => {
-    app.delete('/delnoticias', async (req, res) => {
-        try {
-            const id = ObjectId.createFromHexString(req.body._id)
-            const resultado = await app.DBClient.db('portalnoticias')
-                .collection('noticias')
-                .deleteOne({_id: id})
-            res.status(200).send("Usuário apagado")
-        } catch (error) {
-            res.status(400).send("Erro ao apagar o usuário: " + error)
-        }
-    })
-
-}
+  app.delete("/delnoticias", async (req, res) => {
+    try {
+      const id = ObjectId.createFromHexString(req.body._id);
+      const resultado = await app.DBClient.db("portalnoticias")
+        .collection("noticias")
+        .deleteOne({ _id: id });
+      res.status(200).send("Usuário apagado");
+    } catch (error) {
+      res.status(400).send("Erro ao apagar o usuário: " + error);
+    }
+  });
+};
